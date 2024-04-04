9d3b93342310e60ce2fcb3af4e9d098d08708194
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2024-04-04 08:00:00 +0200

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,42 +1,88 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcosenza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/31 22:55:42 by gcosenza          #+#    #+#             */
-/*   Updated: 2023/10/16 21:38:51 by gcosenza         ###   ########.fr       */
+/*   Created: 2024/04/03 08:33:48 by gcosenza          #+#    #+#             */
+/*   Updated: 2024/04/04 07:59:26 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
 
-int	ft_atoi(const char *nptr)
+#include <unistd.h>
+
+typedef	struct	s_param
+{
+	unsigned int	n_philos;
+	unsigned int	t_die;
+	unsigned int	t_eat;
+	unsigned int	t_sleep;
+	unsigned int	*n_eat;
+}	t_params;
+
+int	check_input(char **argv, int n)
 {
 	int	i;
-	int	num;
-	int	neg;
+	int	j;
+
+	i = 1;
+	while (i < n)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+unsigned int	*ft_atoi(const char *nptr)
+{
+	int	i;
+	unsigned int	*num;
 
 	i = 0;
-	neg = 1;
-	num = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-		i++;
-	if (nptr[i] == '-')
-	{
-		neg = neg * -1;
-		i++;
-	}
-	else if (nptr[i] == '+')
-		i++;
+	*num = 0;
 	while (nptr[i] >= 48 && nptr[i] <= 57)
 	{
-		num = 10 * num + nptr[i] - 48;
+		*num = 10 * *num + nptr[i] - 48;
 		i++;
 	}
-	return (num * neg);
+	return (num);
 }
-/*int	main()
+
+void	set_param(int argc, char **argv, t_params *params)
 {
-	printf("%d", ft_atoi("  \t\n-+--+-+-+-1a456"));	
-}*/
+	(*params).n_philos = *ft_atoi(argv[1]);
+	(*params).t_die = *ft_atoi(argv[2]);
+	(*params).t_eat = *ft_atoi(argv[3]);
+	(*params).t_sleep = *ft_atoi(argv[4]);
+	if (argv[5])
+		(*params).n_eat = ft_atoi(argv[5]);
+	else
+		(*params).n_eat = (void *)NULL;
+}
+
+int	 main(int argc, char **argv)
+{
+	int 		i;
+	t_params	params;
+
+	i = 0;
+	if (argc != 5 && argc != 6)
+	{
+		write(1, "Wrong number of arguments\n", 26);
+		return(1);
+	}
+	if (!check_input(argv, argc)) //all params should be numbers
+	{
+		write(1, "Only positive unsigned int accepted\n", 27);
+		return(1);
+	}
+	set_param(argc, argv, &params); //convert char to numbers and fill the struct
+	return (0);
+}
